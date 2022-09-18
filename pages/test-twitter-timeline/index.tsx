c300e5ff86b4b65561e0729f560326f4fa7cdbd5
山下純一 <metarusword1998@gmail.com>
2022-09-18 19:49:53 +0900

    テスト用ページ追加

---
+++ pages/test-twitter-timeline/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next'
+
+import { TestTwitterTimeLine } from '../../src/components/test-twitter-timeline'
+
+const Page: NextPage = () => <TestTwitterTimeLine />
+
+export default Page
---
+++ src/components/test-twitter-timeline/test-twitter-timeline.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent, useState, useCallback, useEffect, useRef } from 'react'
+
+export const TestTwitterTimeLine: FunctionComponent = () => {
+  return <div>こんにちは！</div>
+}
