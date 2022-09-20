ae56a7cc1c426cb0725081a5cc8649ae0d60b43d
MIYABETaisei <mytspe46@gmail.com>
2022-09-20 23:15:31 +0900

    ✨ ログイン済みのときはタイムラインページへのリンクを表示 #7

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,23 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import { useAuth } from "src/context/auth";
 import { login, logout } from "src/utils/firebase/auth";
 
 const Home: NextPage = () => {
   const { user } = useAuth();
+
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center flex-wrap max-w-[400px]">
       <Image src="/logo.png" width={150} height={150} />
       <h1 className="text-3xl font-bold text-white mt-6">No Spoilers Twitter!!!</h1>
       {user ? (
         <div className="w-2/3">
-          <button
-            className="w-full flex items-center justify-center border border-white px-4 py-3 mt-8 text-lg font-bold text-white rounded-full"
-            onClick={login}
-          >
-            タイムラインを見る
-          </button>
+          <Link href="/timeline">
+            <a className="w-full flex items-center justify-center border border-white px-4 py-3 mt-8 text-lg font-bold text-white rounded-full">
+              タイムラインを見る
+            </a>
+          </Link>
           <button
             className="w-full flex items-center justify-center border border-white px-4 py-3 mt-8 text-lg font-bold text-white rounded-full"
             onClick={logout}
