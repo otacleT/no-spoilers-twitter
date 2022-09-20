938a47384d8d262d13ae3526da7e6bf9180d4f11
MIYABETaisei <mytspe46@gmail.com>
2022-09-20 22:46:03 +0900

    ✨ トップページの簡単なUI実装 #7

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,37 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import { Feed } from 'src/components/Feed';
-import { Mute } from 'src/components/Mute';
-import { Sidebar } from 'src/components/Siderbar';
+import { NextPage } from "next";
+import Image from "next/image";
+import { useAuth } from "src/context/auth";
+import { login, logout } from "src/utils/firebase/auth";
 
 const Home: NextPage = () => {
+  const { user } = useAuth();
   return (
-    <div>
-      <Head>
-        <title>Twitter clone</title>
-      </Head>
-      <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
-        <Sidebar />
-        <Feed />
-        <Mute />
-      </main>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center flex-wrap max-w-[400px]">
+      <Image src="/logo.png" width={150} height={150} />
+      <h1 className="text-3xl font-bold text-white mt-6">No Spoilers Twitter!!!</h1>
+      {user ? (
+        <div className="w-2/3">
+          <button
+            className="w-full flex items-center justify-center border border-white px-4 py-3 mt-8 text-lg font-bold text-white rounded-full"
+            onClick={login}
+          >
+            タイムラインを見る
+          </button>
+          <button
+            className="w-full flex items-center justify-center border border-white px-4 py-3 mt-8 text-lg font-bold text-white rounded-full"
+            onClick={logout}
+          >
+            logout
+          </button>
+        </div>
+      ) : (
+        <button
+          className="w-2/3 flex items-center justify-center border border-white px-4 py-3 mt-8 text-lg font-bold text-white rounded-full"
+          onClick={login}
+        >
+          Twitter login
+        </button>
+      )}
     </div>
   );
 };
