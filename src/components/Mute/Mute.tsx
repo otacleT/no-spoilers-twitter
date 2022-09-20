d8932d756d615425a698da1b70fe7996cd20a3f7
MIYABETaisei <mytspe46@gmail.com>
2022-09-20 21:15:51 +0900

    ✨ モーダル完成（初期値あり）

--- src/components/Mute/Mute.tsx
+++ src/components/Mute/Mute.tsx
@@ -1,6 +1,6 @@
 import { Modal, MultiSelect, Space, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { FC, useState } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { MuteItem } from "../MuteItem";
 
 type Mute = {
@@ -10,29 +10,39 @@ type Mute = {
 
 export const Mute: FC = () => {
   const [opened, setOpened] = useState<boolean>(false);
-  // const [muteList, setMuteList] = useState<
-  // const data = [
-  //   { value: "react", label: "React" },
-  //   { value: "ng", label: "Angular" },
-  //   { value: "svelte", label: "Svelte" },
-  //   { value: "vue", label: "Vue" },
-  //   { value: "riot", label: "Riot" },
-  //   { value: "next", label: "Next.js" },
-  //   { value: "blitz", label: "Blitz.js" },
-  // ];
+  const [muteList, setMuteList] = useState<string[]>([]);
+  const [data, setData] = useState(["testA", "testB", "testC", "testD"]);
 
   const form = useForm({
     initialValues: {
       title: "",
-      muteItem: "",
+      muteList: muteList,
     },
   });
-  const [data, setData] = useState(["Cleaning", "Manual work", "Lifting", "Delivering"]);
-  const handleSubmit = (values: typeof form.values) => {
-    console.log(values.title, values.muteItem);
+
+  const handleCreate = useCallback(
+    (query: string) => {
+      setData([...data, query]);
+      setMuteList([...muteList, query]);
+      return query;
+    },
+    [muteList, data],
+  );
+  const handleChange = useCallback((query: string[]) => {
+    setData([...query]);
+    setMuteList([...query]);
+  }, []);
+  const handleClose = useCallback(() => {
     setOpened(false);
+    setData(["testA", "testB", "testC", "testD"]);
+    setMuteList([]);
     form.reset();
-  };
+  }, []);
+  const handleSubmit = useCallback((values: typeof form.values) => {
+    console.log(values.title, values.muteList);
+    handleClose();
+  }, []);
+
   return (
     <div className="pl-5 pt-4">
       <div className="hidden flex-grow sm:flex sm:w-[350px] items-center justify-between py-2 px-2">
@@ -49,12 +59,7 @@ export const Mute: FC = () => {
         <MuteItem />
         <MuteItem />
       </div>
-      <Modal
-        centered
-        opened={opened}
-        onClose={() => setOpened(false)}
-        title="ミュートしたい要素を追加"
-      >
+      <Modal centered opened={opened} onClose={handleClose} title="ミュートしたい要素を追加">
         <form onSubmit={form.onSubmit(handleSubmit)}>
           <TextInput
             required
@@ -63,22 +68,17 @@ export const Mute: FC = () => {
             {...form.getInputProps("title")}
           />
           <Space h="md" />
-          <TextInput
-            required
-            label="ミュートする要素"
-            placeholder="RED"
-            {...form.getInputProps("muteItem")}
-          />
-          <Space h="xl" />
           <MultiSelect
             searchable
             clearable
             creatable
             getCreateLabel={(query) => `+ Create ${query}`}
-            onCreate={(query) => `${query}`}
+            onCreate={(query) => handleCreate(query)}
+            onChange={handleChange}
             data={data}
-            label="Your favorite frameworks/libraries"
-            placeholder="Pick all that you like"
+            label="ミュートしたい要素"
+            placeholder="選択or入力"
+            {...form.getInputProps("muteList")}
           />
           <Space h="xl" />
           <button className="w-full h-[40px] bg-[#1d9bf0] rounded-full flex items-center justify-center text-sm leading-none text-white">
