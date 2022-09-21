40e0a7536b0c2395156a6bc48059967dd4df7f9d
MIYABETaisei <mytspe46@gmail.com>
2022-09-21 15:59:56 +0900

    ✨ ミュートのオン・オフ実装 #7

--- src/components/MuteItem/MuteSwitch.tsx
+++ src/components/MuteItem/MuteSwitch.tsx
@@ -1,36 +1,45 @@
 import { Badge, Switch } from "@mantine/core";
-import { FC, useCallback, useEffect, useState } from "react";
-import { useAuth } from "src/context/auth";
+import { FC, useCallback, useEffect } from "react";
 import { MuteItem } from "src/types/MuteItem";
 import { editMute } from "src/utils/firebase/editMute";
 
 type Props = {
   muteItem: MuteItem;
+  index: number;
+  handleUpdate: (key: number, newItem: MuteItem) => void;
 };
 
 export const MuteSwitch: FC<Props> = (props) => {
-  const [active, setActive] = useState<boolean>();
-  const { user } = useAuth();
-  const { muteItem } = props;
+  const { muteItem, index, handleUpdate } = props;
 
   const handleSwitch = useCallback(
     (event: boolean) => {
-      setActive(event);
-      editMute({
+      handleUpdate(index, {
+        user: muteItem.user,
         title: muteItem.title,
         muteList: muteItem.muteList,
-        id: muteItem.id,
         mutable: event,
-        user: user,
+        id: muteItem.id,
+      });
+      editMute({
+        user: muteItem.user,
+        title: muteItem.title,
+        muteList: muteItem.muteList,
+        mutable: event,
+        id: muteItem.id,
       });
     },
-    [user],
+    [index, muteItem],
   );
+  // useEffect(() => {
+  //   console.log(muteItem.title, index);
+  // }, [muteItem]);
+
   return (
     <div className="relative py-4 px-3">
       <h5 className="text-2xl text-white opacity-60">{muteItem.title}</h5>
       <Switch
-        checked={active}
+        checked={muteItem.mutable}
         className="absolute top-1/2 right-2 -translate-y-1/2"
         color="twitterColor"
         onChange={(event) => handleSwitch(event.currentTarget.checked)}
