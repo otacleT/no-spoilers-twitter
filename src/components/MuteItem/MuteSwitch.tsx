cf826ce2969f896e1c305ebf44e13b84f2949231
MIYABETaisei <mytspe46@gmail.com>
2022-09-22 05:18:59 +0900

    💄 編集時の表示アニメーション #14

--- src/components/MuteItem/MuteSwitch.tsx
+++ src/components/MuteItem/MuteSwitch.tsx
@@ -1,11 +1,11 @@
-import { MinusCircleIcon } from "@heroicons/react/24/outline";
-import { ChevronRightIcon } from "@heroicons/react/24/solid";
-import { ActionIcon, Badge, Switch } from "@mantine/core";
-import { FC, useCallback, useState } from "react";
-import { useAuth } from "src/context/auth";
-import { MuteItem } from "src/types/MuteItem";
-import { editMute } from "src/utils/firebase/editMute";
-import { EditModal } from "../EditModal";
+import { MinusCircleIcon } from '@heroicons/react/24/outline';
+import { ChevronRightIcon } from '@heroicons/react/24/solid';
+import { ActionIcon, Badge, Switch } from '@mantine/core';
+import { FC, useCallback, useState } from 'react';
+import { useAuth } from 'src/context/auth';
+import { MuteItem } from 'src/types/MuteItem';
+import { editMute } from 'src/utils/firebase/editMute';
+import { EditModal } from '../EditModal';
 
 type Props = {
   muteItem: MuteItem;
@@ -46,39 +46,44 @@ export const MuteSwitch: FC<Props> = (props) => {
 
   return (
     <div
-      className={`w-full relative py-4 px-3 flex flex-wrap items-center justify-start ${
-        isSelect && "cursor-pointer hover:opacity-80"
+      className={`w-full relative py-3 pr-3 flex flex-wrap items-center justify-start ${
+        isSelect && 'cursor-pointer hover:opacity-80'
       }`}
     >
-      {isSelect && (
-        <ActionIcon className="w-[30px] flex justify-center" onClick={() => handleDelete(index)}>
-          <MinusCircleIcon className="w-6 h-6 text-[#ff0000]" />
-        </ActionIcon>
-      )}
-      <div className="w-[calc(100%-30px)]" onClick={() => handleOpen()}>
-        <h5
-          className={`text-2xl text-white opacity-60 ${muteItem.mutable && "opacity-100"} ${
-            isSelect && "pl-3"
-          }`}
-        >
+      <ActionIcon
+        className={`w-[25px] flex justify-center opacity-0 invisible -translate-x-[10px] ${
+          isSelect && 'fadeIn'
+        } `}
+        onClick={() => handleDelete(index)}
+      >
+        <MinusCircleIcon className='w-6 h-6 text-[#ff0000]' />
+      </ActionIcon>
+      <div
+        className={`w-[calc(100%-35px)] ${isSelect && 'translateLeft'}`}
+        onClick={() => handleOpen()}
+      >
+        <h5 className={`text-2xl text-white opacity-60 ${muteItem.mutable && 'opacity-100'}`}>
           {muteItem.title}
         </h5>
-        <div className={`w-full mt-2 ${isSelect && "pl-3"}`}>
+        <div className='w-full mt-2'>
           {muteItem.muteList.map((item) => (
             <Badge key={item}>{item}</Badge>
           ))}
         </div>
       </div>
-      {isSelect ? (
-        <ChevronRightIcon className="w-6 h-6 absolute top-1/2 right-2 -translate-y-1/2 pointer-events-none" />
-      ) : (
+      {!isSelect && (
         <Switch
           checked={muteItem.mutable}
-          className="absolute top-1/2 right-2 -translate-y-1/2"
-          color="twitterColor"
+          className='absolute top-1/2 right-2 -translate-y-1/2'
+          color='twitterColor'
           onChange={(event) => handleSwitch(event.currentTarget.checked)}
         />
       )}
+      <ChevronRightIcon
+        className={`w-6 h-6 absolute top-1/2 right-2 -translate-y-1/2 pointer-events-none opacity-0 invisible translate-x-[0px] ${
+          isSelect && 'translateRight'
+        }`}
+      />
       <EditModal
         index={index}
         opened={isEdit}
