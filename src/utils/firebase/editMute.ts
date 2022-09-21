c9580491b349da5ae6c9d23b582b72bb4b56c475
MIYABETaisei <mytspe46@gmail.com>
2022-09-21 11:13:58 +0900

    ✨ データベースを更新するための関数 #7

---
+++ src/utils/firebase/editMute.ts
@@ -0,0 +1,18 @@
+import { doc, setDoc } from "firebase/firestore";
+import { MuteItem } from "src/types/MuteItem";
+import { db } from "./init";
+
+export async function editMute(edit: MuteItem): Promise<void> {
+  if (!edit.user) return;
+  const docRef = doc(db, `muteContents/${edit.user.uid}/list`, edit.id);
+  await setDoc(
+    docRef,
+    {
+      title: edit.title,
+      muteList: edit.muteList,
+      mutable: edit.mutable,
+      id: edit.id,
+    },
+    { merge: true },
+  );
+}
