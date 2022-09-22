5d7077cc02cb20b2b958a11d9d43baf2295b4593
MIYABETaisei <mytspe46@gmail.com>
2022-09-22 12:35:52 +0900

    🔥 不要なテストコードの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,17 +11,6 @@ const Home: NextPage = () => {
     <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center flex-wrap max-w-[400px]'>
       <Image src='/logo.png' width={150} height={150} />
       <h1 className='text-3xl font-bold text-white mt-6'>No Spoilers Twitter!!!</h1>
-      {JSON.stringify(process.env)}
-      <p>{process.env.NEXT_PUBLIC_FIREBASE_APIKEY}</p>
-      <p>{process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}</p>
-      <p>{process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}</p>
-      <p>{process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BAKET}</p>
-      <p>{process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID}</p>
-      <p>{process.env.NEXT_PUBLIC_FIREBASE_APP_ID}</p>
-      <p>{process.env.NEXT_PUBLIC_BEARER_TOKEN}</p>
-      <p>{process.env.NEXT_PUBLIC_CLIENT_ID}</p>
-      <p>{process.env.NEXT_PUBLIC_CLIENT_SECRET}</p>
-      <p>{process.env.NEXT_PUBLIC_HEAD_URL}</p>
       {user ? (
         <div className='w-2/3'>
           <form method='GET' action='/api/generateAuthUrl'>
